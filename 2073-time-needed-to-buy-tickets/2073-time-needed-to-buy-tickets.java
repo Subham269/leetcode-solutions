dee215cfe4cc4c7146473edb4b462fd18b9a14c7
Subham Saha <subhamranjansaha006@gmail.com>
2026-08-24 02:06:14 +0530

    Time: 0 ms (100%), Space: 42.8 MB (93.23%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,32 +1,14 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) 
     {
-        List<Integer> list=new ArrayList<>(); 
         int count=0;
-        int i=0;
-        for( int x: tickets)
+        for(int i=0;i<tickets.length;i++)
         {
-            list.add(x);
+            if(i<=k)
+            count+=Math.min(tickets[i],tickets[k]);
+            else
+            count+=Math.min(tickets[k]-1,tickets[i]);
         }
-            while(i<=list.size())
-            {
-                if(i==list.size())
-                i=0;
-
-                list.set(i,list.get(i)-1);
-                count++;
-
-                if(list.get(i)==0)
-                {
-                    list.remove(i);
-                    if(i<k)
-                    k--;
-                    else if(i==k)
-                    break;
-                }
-                else
-                i++;
-            }
         return count;
     }
 }
